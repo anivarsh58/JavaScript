cd4713b71cdff3e35c6326d9ce62c0619f24d0ae
anivarsh58 <113866193+anivarsh58@users.noreply.github.com>
2024-09-13 11:50:39 +0000

    Add in object.js

--- files/object.js
+++ files/object.js
@@ -1,8 +1,11 @@
 // Object.create()
 
+ageSymbol = Symbol("symbol")
+
 const obj = {
     key1 : "value1",
-    key2 : "value2"
+    key2 : "value2",
+    [ageSymbol] : 18
 };
 
-console.log(obj.key1, obj["key2"])
+console.log(obj.key1, obj["key2"], obj[ageSymbol])
