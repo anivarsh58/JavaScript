cf298b6f59ca3f940a042389f322d7c3db66cfd5
anivarsh58 <113866193+anivarsh58@users.noreply.github.com>
2024-09-13 11:03:20 +0000

    Add

--- files/array.js
+++ files/array.js
@@ -1,4 +1,6 @@
 const array = Array(1, 2, 3);
+const array2 = Array(4, 5, 6)
+console.log([...array, ...array2])
 
 array.unshift(9)
 console.log(array)
@@ -6,3 +8,6 @@ console.log(array)
 
 array.shift();
 console.log(array);
+
+console.log(array.join(" "))
+
