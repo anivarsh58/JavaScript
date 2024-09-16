633c522b070641e8a73b879551102bee646275df
anivarsh58 <113866193+anivarsh58@users.noreply.github.com>
2024-09-16 10:58:40 +0000

    Add

--- files/foreach.js
+++ files/foreach.js
@@ -1,5 +1,5 @@
 const languages = ["C", "C++", "Java", "Python", "Ruby", "Javascript"]
 
- languages.forEach((value) => {
-    console.log(value)
+ languages.forEach((value, index, array) => {
+    console.log(value, index, "\n", array);
  })
