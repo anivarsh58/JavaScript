93c52000691259b229bb45dab74acebd5982ad0c
anivarsh58 <113866193+anivarsh58@users.noreply.github.com>
2024-09-16 11:28:44 +0000

    Add reduce.js

---
+++ files/reduce.js
@@ -0,0 +1,12 @@
+const myNums = [1, 2, 3];
+
+const initialValue = 0
+
+console.log(myNums.reduce((accumulator, currentValue) => {
+
+    console.log(accumulator, currentValue);
+    return accumulator + currentValue
+
+}, 0))
+
+console.log(myNums.reduce((accumulator, currentValue) => (accumulator + currentValue), 0))
