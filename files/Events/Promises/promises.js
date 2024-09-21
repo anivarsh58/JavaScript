755eca607f7f456217c2c51bf8cbceac79182d78
anivarsh58 <113866193+anivarsh58@users.noreply.github.com>
2024-09-21 14:06:09 +0000

    ok

--- files/Events/Promises/promises.js
+++ files/Events/Promises/promises.js
@@ -3,6 +3,12 @@ const promise = new Promise(function(resolve, reject) {
     setTimeout(function() {
         console.log("Asynchronous code is executed.");
     }, 1000);
+    resolve();
 });
+
+promise.then(function()
+{
+    console.log("Promise consumed.");
+})
 
 
