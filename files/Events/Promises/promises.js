63f1755ae689b0d643c79e6898c4c08d57903c81
anivarsh58 <113866193+anivarsh58@users.noreply.github.com>
2024-09-21 19:27:55 +0000

    add

--- files/Events/Promises/promises.js
+++ files/Events/Promises/promises.js
@@ -91,3 +91,12 @@ catch (error)
 
 }
 };
+
+
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then ((response) => {return response.json})
+.then ((data) => {
+console.log(data);
+})
+.catch((error) => console.log(error));
