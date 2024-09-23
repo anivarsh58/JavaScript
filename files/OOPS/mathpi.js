0efe461a00c818316fbb55c4342e00678ac66c08
anivarsh58 <113866193+anivarsh58@users.noreply.github.com>
2024-09-23 15:18:32 +0000

    Commit.

---
+++ files/OOPS/mathpi.js
@@ -0,0 +1,16 @@
+const chai = {
+    name: 'ginger chai',
+    price: 250,
+    isAvailable: true,
+
+    orderChai: function(){
+        console.log("Ain't no tea here.");
+    }
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+Object.defineProperty(chai, 'name', {
+    // writable: false,
+    enumerable: true,
+});
