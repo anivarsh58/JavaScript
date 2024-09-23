f621697257d1f7519022593d8415add8a741ae77
anivarsh58 <113866193+anivarsh58@users.noreply.github.com>
2024-09-23 04:43:56 +0000

    Commit.

---
+++ files/OOPS/call.js
@@ -0,0 +1,13 @@
+function setUsername(username)
+{
+    // complex DB calls
+    this.username = username;
+}
+
+function createUser(username, email, password)
+{
+    this.email = email;
+    this.password = password;
+
+    setUsername(this, username)
+}
