208f2989fec4404738e2023221e743103ee678d7
anivarsh58 <113866193+anivarsh58@users.noreply.github.com>
2024-09-23 15:22:10 +0000

    Commit.

--- files/OOPS/mathpi.js
+++ files/OOPS/mathpi.js
@@ -16,3 +16,10 @@ Object.defineProperty(chai, 'name', {
 });
 
 console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+for (let [key, value] of Object.entries(chai)) {
+    if (typeof value !== 'function') {
+        console.log(`${key} : ${value}`);
+    }
+}
+
